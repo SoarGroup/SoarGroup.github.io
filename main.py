8840fc7b251319dcafb458e15d5139cf859410c8
Nathan Glenn <garfieldnate@gmail.com>
2024-04-25 15:43:28 -0500

    Better message about tutorials being under construction

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ def define_env(env):
     def tutorial_wip_warning(file_name):
         return (
             "# 🚧 Under Construction 🚧\n The HTML version of the tutorial "
-            "is currently under construction. If you find it difficult to "
-            "read, the PDF of this chapter is available "
-            f"[here](https://github.com/SoarGroup/Soar/releases/download/releases%2F{env.variables['soar_version']}/{file_name})."
+            "is currently under construction; in particular, the figure "
+            "annotations are missing. You may wish to view the PDF version "
+            f"[here](https://github.com/SoarGroup/Soar/releases/download/releases%2F{env.variables['soar_version']}/{file_name}) "
+            "instead."
+
         )
